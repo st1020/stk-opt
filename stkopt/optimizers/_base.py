29a1cca95feb2af49aa46f7125c30b6a5748133c
st1020 <stone_1020@qq.com>
2022-02-12 17:39:24 +0800

    fix: fix type hists error of OptimizerBase, fix log_func_call_history not work in vectorization mode

--- stkopt/optimizers/_base.py
+++ stkopt/optimizers/_base.py
@@ -3,20 +3,19 @@ import functools
 from numbers import Real
 from abc import ABC, abstractmethod
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
-from typing import Any, Callable, Dict, List, Literal, Iterator, Tuple, Optional, Sequence
+from typing import Any, Callable, Dict, Iterator, List, Literal, Optional, Sequence, Tuple, Union
 
 import numpy as np
 
 from ..test_functions import OptTestFunction
 
-FUNC_WRAPPER_MODES = ('common', 'vectorization', 'cached', 'multithreading', 'multiprocessing')
-FUNC_WRAPPER_MODES_TYPE = Literal['common', 'vectorization', 'cached', 'multithreading', 'multiprocessing']
+T_WrapperMode = Literal['common', 'vectorization', 'cached', 'multithreading', 'multiprocessing']
+T_SequenceReal = Union[Sequence[Real], np.ndarray]
 
 
 class OptimizerBase(ABC):
-    def __init__(self, func: Callable, ndim: int = None, lb: Sequence[Real] = None, ub: Sequence[Real] = None,
-                 func_wrapper_mode: FUNC_WRAPPER_MODES_TYPE = 'common',
-                 func_wrapper_options: Optional[Dict[str, Any]] = None,
+    def __init__(self, func: Callable, ndim: int = None, lb: T_SequenceReal = None, ub: T_SequenceReal = None,
+                 func_wrapper_mode: T_WrapperMode = 'common', func_wrapper_options: Optional[Dict[str, Any]] = None,
                  log_iter_history: bool = True, log_func_call_history: bool = True):
         if ndim is None and isinstance(func, OptTestFunction):
             ndim = func.ndim
@@ -35,67 +34,71 @@ class OptimizerBase(ABC):
             raise ValueError('ub must be greater than lb')
 
         self.best_value: Optional[Real] = None
-        self.best_position: Optional[Sequence[Real, ...]] = None
+        self.best_position: Optional[T_SequenceReal] = None
 
         self.log_func_call_history = log_func_call_history
-        self.func_call_history: List[Sequence[Real, Sequence[Real, ...]]] = list()
+        self.func_call_history: List[Tuple[Real, T_SequenceReal]] = list()
         self.log_iter_history = log_iter_history
-        self.iter_history: List[Sequence[Real, Sequence[Real, ...]]] = list()
+        self.iter_history: List[Tuple[Real, T_SequenceReal]] = list()
 
         self.func = self._func_wrapper(func, mode=func_wrapper_mode, options=func_wrapper_options)
 
-    def _func_wrapper(self, func: Callable, mode: FUNC_WRAPPER_MODES_TYPE, options: Optional[Dict[str, Any]] = None):
-        if mode not in FUNC_WRAPPER_MODES:
-            raise ValueError(f'mode must in {FUNC_WRAPPER_MODES!r}')
+    def _func_wrapper(self, func: Callable, mode: T_WrapperMode, options: Optional[Dict[str, Any]] = None):
+        func_wrapper_modes = ('common', 'vectorization', 'cached', 'multithreading', 'multiprocessing')
+        if mode not in func_wrapper_modes:
+            raise ValueError(f'mode must in {func_wrapper_modes!r}')
 
         if options is None:
             options = {}
 
         if mode == 'vectorization':
+            @functools.wraps(func)
             def wrapper(xx):
+                if self.log_func_call_history and self.best_value is not None:
+                    self.func_call_history.append((self.best_value, self.best_position))
                 return func(xx.T)
 
             return wrapper
+        elif mode == 'cached':
+            @functools.wraps(func)
+            @functools.lru_cache(**options)
+            def wrapper(xx):
+                if self.log_func_call_history and self.best_value is not None:
+                    self.func_call_history.append((self.best_value, self.best_position))
+                return func(xx)
+
+            return lambda x: np.array(list(map(lambda xx: wrapper(tuple(xx)), x)))
         elif mode == 'multiprocessing':
             if self.log_func_call_history:
                 warnings.warn('in multiprocessing mode, log_func_call_history will not work')
             executor = ProcessPoolExecutor(**options)
             return lambda x: np.array(list(executor.map(func, x)))
-
-        if mode == 'cached':
+        else:
             @functools.wraps(func)
-            @functools.lru_cache(**options)
-            def func_cached(xx):
+            def wrapper(xx):
+                if self.log_func_call_history and self.best_value is not None:
+                    self.func_call_history.append((self.best_value, self.best_position))
                 return func(xx)
 
-        @functools.wraps(func)
-        def wrapper(xx):
-            if self.log_func_call_history and self.best_value is not None:
-                self.func_call_history.append((self.best_value, self.best_position))
-            if mode == 'cached':
-                return func_cached(tuple(xx))
-            return func(xx)
+            if mode == 'multithreading':
+                executor = ThreadPoolExecutor(**options)
+                return lambda x: np.array(list(executor.map(wrapper, x)))
+            else:
+                return lambda x: np.array(list(map(wrapper, x)))
 
-        if mode == 'multithreading':
-            executor = ThreadPoolExecutor(**options)
-            return lambda x: np.array(list(executor.map(wrapper, x)))
-        else:
-            return lambda x: np.array(list(map(wrapper, x)))
-
-    def update_best(self, best_value: Real, best_position: Sequence[Real]):
-        if self.best_value is None or self.best_value > best_value:
-            self.best_value = best_value
-            self.best_position = best_position
+    def update_best(self, best_value: Real, best_position: T_SequenceReal):
+        self.best_value = best_value
+        self.best_position = best_position
 
     def log_history(self):
         if self.log_iter_history:
             self.iter_history.append((self.best_value, self.best_position))
 
-    def run(self, *args, **kwargs) -> Tuple[Real, Sequence[Real]]:
+    def run(self, *args, **kwargs) -> Tuple[Real, T_SequenceReal]:
         for _ in self.run_iter(*args, **kwargs):
             pass
         return self.best_value, self.best_position
 
     @abstractmethod
-    def run_iter(self, *args, **kwargs) -> Iterator[Tuple[Real, Sequence[Real]]]:
+    def run_iter(self, *args, **kwargs) -> Iterator[Tuple[Real, T_SequenceReal]]:
         pass
--- stkopt/optimizers/pso.py
+++ stkopt/optimizers/pso.py
@@ -1,18 +1,17 @@
 from numbers import Real
-from typing import Any, Callable, Dict, Optional, Sequence, Iterator, Tuple
+from typing import Any, Callable, Dict, Iterator, Optional, Sequence, Tuple
 
 import numpy as np
 
-from ._base import OptimizerBase, FUNC_WRAPPER_MODES_TYPE
+from ._base import OptimizerBase, T_WrapperMode, T_SequenceReal
 
 
 class PSO(OptimizerBase):
-    def __init__(self, func: Callable, ndim: int = None, lb: Sequence[Real] = None, ub: Sequence[Real] = None,
-                 func_wrapper_mode: FUNC_WRAPPER_MODES_TYPE = 'common',
-                 func_wrapper_options: Optional[Dict[str, Any]] = None,
+    def __init__(self, func: Callable, ndim: int = None, lb: T_SequenceReal = None, ub: T_SequenceReal = None,
+                 func_wrapper_mode: T_WrapperMode = 'common', func_wrapper_options: Optional[Dict[str, Any]] = None,
                  log_iter_history: bool = True, log_func_call_history: bool = True,
                  pop_size: int = 40, max_iter: int = 150, w: Real = 0.8, c1: Real = 0.5, c2: Real = 0.5,
-                 v_max: Optional[Sequence[Real]] = None):
+                 v_max: Optional[T_SequenceReal] = None):
         super().__init__(func, ndim, lb, ub, func_wrapper_mode, func_wrapper_options, log_iter_history,
                          log_func_call_history)
         self.pop_size = pop_size
@@ -22,39 +21,56 @@ class PSO(OptimizerBase):
         self.c2 = c2
         if v_max is None:
             v_max = self.ub - self.lb
-        elif isinstance(v_max, Sequence):
+        elif isinstance(v_max, Sequence) or isinstance(v_max, np.ndarray):
             if len(v_max) != self.ndim:
                 raise ValueError('length of v_max must equal to ndim')
         else:
-            raise TypeError(f'v_max must be NoneType or Sequence[Real], not {type(v_max)}')
+            raise TypeError(f'v_max must be NoneType, Sequence[Real] or np.ndarray, not {type(v_max)}')
         self.v_max = v_max
 
-    def run_iter(self) -> Iterator[Tuple[Real, Sequence[Real]]]:
-        # init population
-        positions = np.random.uniform(low=self.lb, high=self.ub, size=(self.pop_size, self.ndim))
-        values = self.func(positions).reshape(-1, 1)
-        velocities = np.random.uniform(low=-self.v_max, high=self.v_max, size=(self.pop_size, self.ndim))
-        p_best_values = values.copy()
-        p_best_positions = positions.copy()
-        index_min = p_best_values.argmin()
-        self.update_best(best_value=p_best_values[index_min][0], best_position=positions[index_min, :].copy())
+        self.positions: Optional[np.ndarray] = None
+        self.values: Optional[np.ndarray] = None
+        self.velocities: Optional[np.ndarray] = None
+        self.p_best_values: Optional[np.ndarray] = None
+        self.p_best_positions: Optional[np.ndarray] = None
 
+    def init_population(self):
+        self.positions = np.random.uniform(low=self.lb, high=self.ub, size=(self.pop_size, self.ndim))
+        self.values = self.func(self.positions).reshape(-1, 1)
+        self.velocities = np.random.uniform(low=-self.v_max, high=self.v_max, size=(self.pop_size, self.ndim))
+        self.p_best_values = self.values.copy()
+        self.p_best_positions = self.positions.copy()
+        self.update_global_beat()
+
+    def update_velocities(self):
+        self.velocities = self.w * self.velocities + \
+                          self.c1 * np.random.rand(self.pop_size, self.ndim) * (self.best_position - self.positions) + \
+                          self.c2 * np.random.rand(self.pop_size, self.ndim) * (self.p_best_positions - self.positions)
+
+    def update_positions(self):
+        self.positions = np.clip(self.positions + self.velocities, self.lb, self.ub)
+
+    def update_values(self):
+        self.values = self.func(self.positions).reshape(-1, 1)
+
+    def update_partial_best(self):
+        need_update = self.p_best_values > self.values
+        self.p_best_positions = np.where(need_update, self.positions, self.p_best_positions)
+        self.p_best_values = np.where(need_update, self.values, self.p_best_values)
+
+    def update_global_beat(self):
+        index_min = self.p_best_values.argmin()
+        best_value = self.p_best_values[index_min][0]
+        if self.best_value is None or self.best_value > best_value:
+            self.update_best(best_value=best_value, best_position=self.positions[index_min, :].copy())
+
+    def run_iter(self) -> Iterator[Tuple[Real, Sequence[Real]]]:
+        self.init_population()
         for _ in range(self.max_iter):
-            # update velocities
-            velocities = self.w * velocities + \
-                         self.c1 * np.random.rand(self.pop_size, self.ndim) * (self.best_position - positions) + \
-                         self.c2 * np.random.rand(self.pop_size, self.ndim) * (p_best_positions - positions)
-            # update positions
-            positions = np.clip(positions + velocities, self.lb, self.ub)
-            # update values
-            values = self.func(positions).reshape(-1, 1)
-            # update partial best
-            need_update = p_best_values > values
-            p_best_positions = np.where(need_update, positions, p_best_positions)
-            p_best_values = np.where(need_update, values, p_best_values)
-            # update global best
-            index_min = p_best_values.argmin()
-            self.update_best(best_value=p_best_values[index_min][0], best_position=positions[index_min, :].copy())
-            # log history
+            self.update_velocities()
+            self.update_positions()
+            self.update_values()
+            self.update_partial_best()
+            self.update_global_beat()
             self.log_history()
             yield self.best_value, self.best_position
